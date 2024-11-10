876fe4cac97067c1b4124cac92e5c6c51aa60732
yukinaka <yuki.n.0918@gmail.com>
2024-11-10 16:47:34 +0900

    finish pointers

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module go-tdd
 go 1.23.2
 
 require (
+	github.com/kisielk/errcheck v1.8.0 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
 	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/tools v0.26.0 // indirect
 )
---
+++ pointerserrors/pointerserrors_test.go
@@ -0,0 +1,54 @@
+package pointerserrors
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBalance(t, wallet, Bitcoin(10))
+
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, err error, want error) {
+	t.Helper()
+	if err == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+	if !errors.Is(err, want) {
+		t.Errorf("got %q, want %q", err, want)
+	}
+}
